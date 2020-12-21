bb863681fbba76396c4265a4aa158ab5bd299160
Allen Sparks <m4sparks27@gmail.com>
2020-12-21 16:59:47 -0600

    fixed the broken <headercontent > issue by placing its content inline on layout.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
+/*This page provides access to all pages, good for passing data to all parts of the app*/
+/*This is where you would also place the layout*/
+//Even though layout is an html element here it renders the whole thing from Layout.js
 import App from "next/app";
+import Layout from "../components/_App/Layout"
 
 class MyApp extends App {
   render() {
     const { Component } = this.props;
-    return <Component />;
+    
+    return (
+      <Layout>
+        <Component/>
+      </Layout>
+    );
   }
 }
 
--- pages/account.js
+++ pages/account.js
@@ -1,5 +1,5 @@
 function Account() {
-  return <>account</>;
+  return <>Supaccount</>;
 }
 
 export default Account;
