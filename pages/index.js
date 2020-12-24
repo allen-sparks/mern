a2452afd38557d0cda778e19248a93eba035abc0
Allen Sparks <m4sparks27@gmail.com>
2020-12-24 00:15:52 -0600

    Added half an api route

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+
 function Home() {
+  //The array argument has 'dependcies' which when changed will trigger this function (which is about getting api calls)
+  //if the array is blank it will just run when ever the component is mounted››
+  React.useEffect(() => {
+    getProducts()
+  }, [])
+
+  function getProducts() {
+    const url = 'http://localhost:3000/api/products'
+    axios.get(url)
+  }
+
   return <>home</>;
 }
 
